94acb462665d85c727be1f61fe7a378d5b9ff779
imagi <imagi@Imagine>
2017-09-24 17:41:33 +0900

    dd

--- hangout/src/main/java/com/meet/together/user/dao/UserDaoImpl.java
+++ hangout/src/main/java/com/meet/together/user/dao/UserDaoImpl.java
@@ -13,9 +13,6 @@ public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao {
 	@Override
 	//회원가입 userinfo형식으로 받고 int로 반환
 	public int signupUser(UserInfo ui) {
-		return this.getSqlSession().insert("user.USER_SIGUP", ui);
+		return this.getSqlSession().insert("user.USER_SIGNUP", ui);
 	}
-
-
-
 }
