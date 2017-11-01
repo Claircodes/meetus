86bf4df348d9d0d814ea0d5bfd387f683019e9e7
claircodes <imagine.codes@gmail.com>
2017-11-01 12:09:44 +0900

    t

--- hangout/src/main/java/com/meet/together/common/utils/SessionListener.java
+++ hangout/src/main/java/com/meet/together/common/utils/SessionListener.java
@@ -1,22 +1,17 @@
 package com.meet.together.common.utils;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-import org.apache.log4j.Logger;
-
 public class SessionListener implements HttpSessionListener {
 
-	protected static final Logger log = Logger.getLogger(SessionListener.class);
-
+	
 	public void sessionCreated(HttpSessionEvent event) {
-		log.info("Session ID => "+event.getSession().getId() + ", start time =>" + (new Date()).toString());
+		//log.info("Session ID => "+event.getSession().getId() + ", start time =>" + (new Date()).toString());
 	}
 
 	public void sessionDestroyed(HttpSessionEvent event) {
-		log.info("Session ID => "+event.getSession().getId() + ", end time =>" + (new Date()).toString());
+		//log.info("Session ID => "+event.getSession().getId() + ", end time =>" + (new Date()).toString());
 	}
 
 }
