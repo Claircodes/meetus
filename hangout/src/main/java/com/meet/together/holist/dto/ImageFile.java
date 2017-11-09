f6ef68a0c16f9ca501e88b2625c0bafdf86afad2
Claircodes <imagine.codes@gmail.com>
2017-11-09 18:11:24 +0900

    dd

--- hangout/src/main/java/com/meet/together/holist/dto/ImageFile.java
+++ hangout/src/main/java/com/meet/together/holist/dto/ImageFile.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ImageFile {
 	
-	public static final String IMAGE_DIR = "resources\\client_img\\";
+	public static final String IMAGE_DIR = "\\resources\\client_img\\";
 //	public static final String IMAGE_DIR = "C:\\Users\\imagine\\git\\meetus\\hangout\\src\\main\\webapp\\resources\\client_img\\";
 ///git/meetus/hangout/src/main/webapp/resources/client_img/
  
